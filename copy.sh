dee0ccefcd1c3ff0398c12f62c43a2a5f2d7a711
吉井　孝尚 <tyoshii@MBP-15JAH-032.local>
2015-08-19 15:58:57 +0900

    add bash_profile

--- copy.sh
+++ copy.sh
@@ -10,6 +10,7 @@ PWD=`pwd`
 
 ln -s $PWD/_vimrc ~/.vimrc
 ln -s $PWD/_bashrc ~/.bashrc
+ln -s $PWD/_bash_profile ~/.bash_profile
 ln -s $PWD/_gitconfig ~/.gitconfig
 ln -s $PWD/_gitignore ~/.gitignore
 
