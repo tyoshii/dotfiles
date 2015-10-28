a33610af9b929fdcae31384c3c4d0ac6d844c3b5
Takanao Yoshii <tyoshii@yahoo-corp.jp>
2015-10-28 13:40:20 +0900

    install neobundle

--- copy.sh
+++ copy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# rm old file/dir
 if [ -e ~/.vimrc ]; then
     rm ~/.vimrc
 fi
@@ -21,6 +22,12 @@ fi
 
 PWD=`pwd`
 
+# install neobundle
+curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh > install.sh
+sh ./install.sh
+rm install.sh
+
+# symbolic link
 ln -s $PWD/_vimrc ~/.vimrc
 ln -s $PWD/_bashrc ~/.bashrc
 ln -s $PWD/_bash_profile ~/.bash_profile
@@ -31,4 +38,5 @@ mkdir -p ~/.vim/
 ln -s $PWD/_vim/filetype.vim ~/.vim/filetype.vim
 ln -s $PWD/_vim/syntax ~/.vim/syntax
 
+# about golang
 sh golang.sh
